257ab293445ea11c89dbad11a4af04df4e4d2148
GJhunter <GJ000@outlook.com>
2016-04-22 16:50:34 +0800

    test

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,15 @@ from ..models import User,Permission,Role,Post,Comment
 from ..decorators import admin_required,permission_required
 from flask.ext.login import login_required,current_user
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
 
 @main.route('/edit_profile',methods=['GET','POST'])
 @login_required

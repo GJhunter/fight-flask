9ee69d8e3f4abed8d7abcac34605a99b34187250
GJhunter <GJ000@outlook.com>
2016-04-22 19:45:57 +0800

    performance monitor and logger

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,17 @@ from .. import db
 from ..models import User,Permission,Role,Post,Comment
 from ..decorators import admin_required,permission_required
 from flask.ext.login import login_required,current_user
+from flask.ext.sqlalchemy import get_debug_queries
+
+@main.after_app_request
+def after_request(response):
+	for query in get_debug_queries():
+		if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+			current_app.logger.warning(
+				'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+				% (query.statement,query.parameters,query.duration,
+					query.context))
+	return response
 
 @main.route('/shutdown')
 def server_shutdown():
--- config.py
+++ config.py
@@ -3,14 +3,21 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	SQLALCHEMY_RECORD_QUERIES = True
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flasky Admin <gj00403@gmail.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 	FLASKY_POSTS_PER_PAGE = 20
 	FLASKY_FOLLOWERS_PER_PAGE = 50
 	FLASKY_COMMENTS_PER_PAGE = 30
+	FLASKY_SLOW_DB_QUERY_TIME=0.5
 	
 	@staticmethod
 	def init_app(app):#静态方法，为了可以直接用类进行调用
@@ -18,11 +25,6 @@ class Config:
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
@@ -35,6 +37,28 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+	@classmethod
+	def init_app(cls,app):
+		Config.init_app(app)
+
+		import logging
+		from logging.handlers import SMTPHandler
+		credentials = None
+		secure = None
+		if getattr(cls,"MAIL_USERNAME",None) is not None:
+			credentials = (cls.MAIL_USERNAME,cls.MAIL_PASSWORD)
+			if getattr(cls,'MAIL_USE_TLS',None):
+				secure = ()
+		mail_handler = SMTPHandler(
+			mailhost=(cls.MAIL_SERVER,cls.MAIL_PORT),
+			fromaddr=cls.FLASKY_MAIL_SENDER,
+			toaddrs=[cls.FLASKY_ADMIN],
+			subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + 'Application Error',
+			credentials=credentials,
+			secure=secure)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
 
 config = {
 	'development':DevelopmentConfig,
